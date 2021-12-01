b9fe2d5066df516f9f241f2baef84fc52941bd61
Xetcer <Ferdinand_tema@mail.ru>
2021-12-01 21:31:26 +0500

    add activity and apply buttons

--- app/src/main/java/xetzer/TargetProject/MainActivity.kt
+++ app/src/main/java/xetzer/TargetProject/MainActivity.kt
@@ -1,11 +1,31 @@
 package xetzer.TargetProject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    lateinit var addTargetBtn: FloatingActionButton
+    lateinit var closeApp: FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        BindWidgets()
     }
+
+    // secondActivityIntent.putExtra(HELLO_KEY, "Hello from main activity")
+    private fun BindWidgets() {
+        addTargetBtn = findViewById(R.id.addTarget_btn)
+        addTargetBtn.setOnClickListener {
+            val secondActivityIntent: Intent = Intent(this, AddTargetActivity::class.java)
+            startActivity(secondActivityIntent)
+        }
+        closeApp = findViewById(R.id.closeApp_btn)
+        closeApp.setOnClickListener {
+            finish()
+        }
+    }
+
+
 }
---
+++ app/src/main/java/xetzer/TargetProject/TargetClass.kt
@@ -0,0 +1,10 @@
+package xetzer.TargetProject
+
+import java.time.chrono.ChronoZonedDateTime
+import java.util.*
+
+// Класс для работы с целью
+data class TargetClass(
+     val target: String,        // Цель
+     val date: Date             // Дата постановки
+)
