7927a4e37946ae63f911dd41e45cc0fa220c4c39
Xetcer <Ferdinand_tema@mail.ru>
2021-12-04 12:28:56 +0500

    Init commit\

--- app/src/main/java/xetzer/TargetProject/AddTargetActivity.kt
+++ app/src/main/java/xetzer/TargetProject/AddTargetActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.inputmethod.EditorInfo
 import android.widget.EditText
-
+// переходим в новую веткиу
 class AddTargetActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
